fbd927adb968f4a7c2bbfd82a04d91d3d475dd5a
DanielK-Carleton <63213671+DanielK-Carleton@users.noreply.github.com>
2021-01-26 11:51:21 -0500

    Added in the old training method
    Old training method now available under old_train. Works as in the master branch, only the ratio of selected bots has an input that can be changed. Doesn't actually work if changed, though, because that would need us to reuse the selected bots and there isn't code for that right now. Contains commented out code that might solve that problem, but it's untested.

--- python/trainer.py
+++ python/trainer.py
@@ -26,6 +26,95 @@ class Trainer:
         for bot in self.bot_names:
             initialize_new_bot(bot)
 
+    def old_train(self, mutation_percent, generations, games_per_bot, fast_count, bots_per_sim=0, delete_files=True,
+              operator_probability='50', carryover_portion=0.5):
+        """
+        Trains the bots for a set number of generations. Each generation half the bots are selected to move on to the next generation
+        and the other half are thrown out. The thrown out bots are replaced by a combination of cross over and mutation on the selected
+        bots dictated by the mutation percent parameter.
+        mutation_percent: percentage of new bots generated via mutation (the rest are generated via crossover)
+        generations: number of generations to train for
+        games_per_bot: number of games each bot plays each generation
+        fast_count: number of fast bots in each simulation (smart bots will be 3 - fast bots)
+        bots_per_sim: Number of bots to include in each simulation, default is all bots in one simulation
+        operator_probability = probability of making an operator on a mutation (string number from 0 - 100)
+        carryover_portion: portion of bots to carry over (float from 0 to 1) TODO: make this actually work if it's not set to 0.5
+        """
+
+        for gen in range(generations):
+            self.gen_count += 1
+
+            # check to see if we are doing default number of games per simulation
+            if bots_per_sim == 0:
+                bots_per_sim = self.bot_count
+
+            # set up list of simulations
+            simulations = []
+            simulation_count = self.bot_count // bots_per_sim
+            for i in range(simulation_count):
+                sim_name = self.bot_prefix + "_generation_" + str(self.gen_count) + '_' + str(i + 1)
+                simulations.append(
+                    Simulation(sim_name, self.bot_names[i * bots_per_sim: i * bots_per_sim + bots_per_sim],
+                               games_per_bot, fast_count, delete_files))
+
+            if self.bot_count % bots_per_sim != 0:
+                bots_left_over = self.bot_count - (simulation_count * bots_per_sim)
+                final_sim_name = self.bot_prefix + "_generation_" + str(self.gen_count) + '_' + str(
+                    simulation_count + 1)
+                simulations.append(
+                    Simulation(final_sim_name, self.bot_names[-bots_left_over:], games_per_bot, fast_count,
+                               delete_files))
+
+            # run simulations, update results
+            self.results[self.gen_count] = {}
+            for simulation in simulations:
+                simulation.simulate()
+                res = simulation.get_evo_results()
+
+                for key in res:
+                    self.results[self.gen_count][key] = res[key]
+
+            # Selects bots to move on and bots to be overwritten
+            gen_results = [(k, self.results[self.gen_count][k]) for k in self.results[self.gen_count]]
+            gen_results.sort(key=lambda x: x[1], reverse=True)
+            selected_bots = gen_results[:(len(gen_results)*carryover_portion)]
+            bad_bots = gen_results[(len(gen_results)*carryover_portion):]
+            random.shuffle(selected_bots)
+            random.shuffle(bad_bots)
+
+            # calculate number of bots to mutate/cross over. cross over count must be even
+            mutate_count = round(mutation_percent * len(selected_bots))
+            cross_over_count = len(selected_bots) - mutate_count
+            if cross_over_count % 2 != 0:
+                if mutate_count == 0:
+                    cross_over_count -= 1
+                    mutate_count += 1
+                else:
+                    cross_over_count += 1
+                    mutate_count -= 1
+
+            # mutate bots
+            for i in range(mutate_count):
+                #if len(selected_bots) == 0:
+                    #selected_bots = gen_results[:(len(gen_results)*carryover_portion)]
+                    #random.shuffle(selected_bots)
+                bot_to_mutate = selected_bots.pop()[0]
+                bot_to_replace = bad_bots.pop()[0]
+                mutate_bot(bot_to_mutate, bot_to_replace, operator_probability)
+
+            # Cross over bots
+            for i in range(cross_over_count // 2):
+                #if len(selected_bots) == 0:
+                    #selected_bots = gen_results[:(len(gen_results)*carryover_portion)]
+                    #random.shuffle(selected_bots)
+                bot1_to_cross_over = selected_bots.pop()[0]
+                bot2_to_cross_over = selected_bots.pop()[0]
+                bot1_to_replace = bad_bots.pop()[0]
+                bot2_to_replace = bad_bots.pop()[0]
+                cross_over(bot1_to_cross_over, bot2_to_cross_over, bot1_to_replace, bot2_to_replace)
+                
+                
+                
     def train(self, mutation_percent, generations, games_per_bot, fast_count, bots_per_sim=0, delete_files=True, operator_probability='50'):
         """
         Trains the bots for a set number of generations. Each generation, the highest-performing bots are used to create the majority of the
